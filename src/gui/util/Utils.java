f2e3598c960fbf641286d7dda6bd7c98b6dc40b9
Ana Claudia <cacaucosta88@yahoo.com.br>
2022-08-13 19:50:09 -0300

    Observer design pattern to update tableview

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -12,7 +12,7 @@ public class Utils {
 	
 	public static Integer tryParseToInt(String str) {
 		try {
-		return Integer.parseInt(str);
+			return Integer.parseInt(str);
 		}
 		catch (NumberFormatException e) {
 			return null;
